42c52ebf0c09b53dbd7f908767472d01b18a5199
athaimahbub <athaimahbub2016@gmail.com>
2023-05-03 10:58:15 +0600

    Header added

--- src/layouts/Main.jsx
+++ src/layouts/Main.jsx
@@ -1,10 +1,14 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
+import Header from '../pages/shared/Header/Header';
+import Footer from '../pages/shared/Footer/Footer';
 
 const Main = () => {
     return (
         <div>
-            
+            <Header></Header>
+
+            <Footer></Footer>
         </div>
     );
 };
